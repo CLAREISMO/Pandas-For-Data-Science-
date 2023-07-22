15b9dca40839ac35fb9b33df6612d7b95c36268c
Claudia Isabel Reyes Moreno <63759427+CLAREISMO@users.noreply.github.com>
2023-07-22 16:08:17 -0500

    Update Class2_Indexing_Selecting_Assigning.py

--- Class2_Indexing_Selecting_Assigning.py
+++ Class2_Indexing_Selecting_Assigning.py
@@ -161,4 +161,116 @@ print(dfMay.Catalog == 'Moments')
 import pandas as pd
 
 dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
-print(dfMay.TP == '538') # this 
+print(dfMay.TP == '538') # this numerical data was stored as text!!!
+
+#* To know the percentage of occurrence of a value
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+print(dfMay.loc[dfMay.TP == '538']) # this numerical data was stored as text!!!
+print((dfMay.loc[dfMay.TP == '538']).shape)
+
+#* Join two queries through &
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+print(dfMay.loc[(dfMay.TP == '538')  & (dfMay.Weight  <= '0.4')]) # this numerical data was stored as text!!!
+
+#* Join two queries through | 
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+print(dfMay.loc[(dfMay.TP == '538')  | (dfMay.Weight  <= '0.4')]) # this numerical data was stored as text!!!
+
+#* isin method
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+filter1 = dfMay["Catalog"].isin(["Moments"])
+print(filter1)
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+filter2 = dfMay["Catalog"].isin(["Moments", "", "Rozana", "Surmaya" ])
+print(filter2)
+
+#* Applying the iloc selector together with the isin method
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+filter_list = ['Moments', 'Rozana', 'Surmaya']
+new_df = dfMay.loc[dfMay['Catalog'].isin(filter_list)]
+print(new_df)
+
+
+#* Filtering data with the isin method and operator(~)
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+filter_list = ['Moments', 'Rozana', 'Surmaya']
+new_df = dfMay.loc[~dfMay['Catalog'].isin(filter_list)]
+print(new_df)
+
+#* isnull method
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+
+print(dfMay.loc[dfMay.Catalog.isnull()])
+print((dfMay.loc[dfMay.Catalog.isnull()]).shape)
+
+#* notnull  method
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+
+print(dfMay.loc[dfMay.Catalog.notnull()])
+print((dfMay.loc[dfMay.Catalog.notnull()]).shape)
+
+#* Null Register check in DF**
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+print(pd.isnull(dfMay))
+
+#* Not-Null with Notna Register check in DF **
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+print(dfMay.notna())
+
+#* Assigning data
+
+import pandas as pd
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+
+dfMay['critic'] = 'everyone'
+print(dfMay['critic'])
+print(dfMay)
+
+import pandas as pd
+
+#* Iterable data Assignment 
+
+dfMay = pd.read_csv('May_2022.csv', index_col = 0) 
+
+dfMay['index_backwards'] = range(len(dfMay), 0, -1)
+print(dfMay['index_backwards'])
+
+print(dfMay)
+
+
+
+
+
