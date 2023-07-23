3f0c2cff59a8e72239b7a3b93df6baaca33c8ade
Claudia Isabel Reyes Moreno <63759427+CLAREISMO@users.noreply.github.com>
2023-07-22 23:41:53 -0500

    Update 3.Summary_Functions_Maps.py

--- 3.Summary_Functions_Maps.py
+++ 3.Summary_Functions_Maps.py
@@ -31,3 +31,52 @@ dfEmer = pd.read_csv('population.csv', index_col = 0)
 print(dfEmer)
 print(type(dfEmer))
 print(dfEmer.N_HIJOS_MENORES_15ANOS.describe())
+
+
+
+#* mean() function
+import pandas as pd
+import numpy as np
+
+dfEmer = pd.read_csv('population.csv', index_col = 0) 
+print(dfEmer)
+print(type(dfEmer))
+print(dfEmer.N_HIJOS_MENORES_15ANOS.mean())
+
+
+
+#* unique() function
+
+import pandas as pd
+import numpy as np
+
+dfEmer = pd.read_csv('population.csv', index_col = 0) 
+print(dfEmer)
+print(type(dfEmer))
+print(dfEmer.N_HIJOS_MENORES_15ANOS.unique())
+
+
+
+#* VALUE_COUNTS()
+import pandas as pd
+import numpy as np
+
+dfEmer = pd.read_csv('population.csv', index_col = 0) 
+print(dfEmer)
+print(type(dfEmer))
+print(dfEmer.N_HIJOS_MENORES_15ANOS.value_counts())
+
+
+
+#* map() method
+import pandas as pd
+import numpy as np
+
+dfEmer = pd.read_csv('population.csv', index_col = 0) 
+print(dfEmer)
+print(type(dfEmer))
+print(dfEmer.N_HIJOS_MENORES_15ANOS.mean())
+
+dfEmer_N_HIJOS_MENORES_15ANOS_mean = dfEmer.N_HIJOS_MENORES_15ANOS.mean()
+dfEmer.N_HIJOS_MENORES_15ANOS.map(lambda p: p - dfEmer_N_HIJOS_MENORES_15ANOS_mean)
+print(dfEmer_N_HIJOS_MENORES_15ANOS_mean)
