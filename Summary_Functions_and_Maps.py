f0348a380d936e37153c5eb8d749d48ac8fc6902
Claudia Isabel Reyes Moreno <63759427+CLAREISMO@users.noreply.github.com>
2023-07-22 22:23:11 -0500

    Create Summary_Functions_and_Maps.py

---
+++ Summary_Functions_and_Maps.py
@@ -0,0 +1,35 @@
+
+#* General features of our  new dataset
+#* Describe() method in not_numeric values
+
+import pandas as pd
+import numpy as np
+
+dfInt = pd.read_csv('International_sale_Report.csv', index_col = 0)
+print(dfInt)
+print(type(dfInt))
+
+print(dfInt.Size.describe())
+print(type('Size'))
+
+print(dfInt.CUSTOMER.describe())
+print(type('CUSTOMER'))
+
+print(dfInt.DATE.describe())
+print(type('Date'))
+
+print(dfInt.PCS.describe())
+print(type('PCS'))
+
+
+#* Describe() method in numeric values
+
+import pandas as pd
+import numpy as np
+
+dfEmer = pd.read_csv('population.csv', index_col = 0) 
+print(dfEmer)
+print(type(dfEmer))
+print(dfEmer.N_HIJOS_MENORES_15ANOS.describe())
+
+
